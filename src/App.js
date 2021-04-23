c166ae8a542717888dbbfd266121a0260a5c62fe
cparros <celparros@gmail.com>
2021-04-23 00:24:46 -0400

    added button component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import TableHeader from './components/TableHeader'
+import FilterButtons from './components/FilterButtons'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FilterButtons />
       <TableHeader />
     </div>
   );
---
+++ src/components/FilterButtons.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FilterButtons () {
+  return (
+  <div>
+    <button>Filter by Name</button><button>Filter by Gender</button>
+  </div>
+  )
+}
+
+export default FilterButtons
